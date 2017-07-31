bd0f4e5eb7c70cbd8040b228a4374b342a21a0e5
hinx <b2765270@gmail.com>
2017-08-01 01:43:21 +0800

    update config

--- src/config.js
+++ src/config.js
@@ -1,5 +1,6 @@
 export default {
-  url: 'http://localhost/submissions.json',
+  // url: 'http://localhost/submissions.json',
+  url: 'https://coscup.org/2017-assets/json/submissions.json',
   begin: '08:40',  // begin time of timeLine
   ending: '17:00'  // end time of timeLine
 }
