74bc001c5c838b6db6c1900da8e9e262bb3486f3
hinx <b2765270@gmail.com>
2017-08-01 04:32:35 +0800

    update constants

--- src/constants.js
+++ src/constants.js
@@ -4,9 +4,11 @@ export const HOUR_WIDTH = 170;  // in minutes
 export const ROOM_COL_WIDTH = 50;
 export const SESSION_HEIGHT = 90;
 export const SUBJECT_MIN_LENGTH = 15;
+export const STATUS_BAR_HEIGHT = 20;
 export const BACKGROUND_COLOR = '#282c34';
+export const DETAIL_HEADER_COLOR = '#f6f6f6';
 export const ROOM_COL_TITLE_COLOR = '#969ea4';
 export const TIME_ROW_TITLE_COLOR = '#969ea4';
 
 // Modal IDs
-export const MODAL_DATE_PICKER = 'DatePickerDialog'
+export const MODAL_DATE_PICKER = 'DatePickerDialog';
